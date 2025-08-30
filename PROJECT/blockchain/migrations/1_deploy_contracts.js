31cb32383b6ca83644743bc3941ac14b8dd66d45
SOUNAK1236 <sounakmaji9@gmail.com>
2025-08-30 15:12:52 +0530

    Delete PROJECT directory

--- PROJECT/blockchain/migrations/1_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const OrganicFoodTraceability = artifacts.require("OrganicFoodTraceability");
-
-module.exports = function (deployer, network, accounts) {
-  // Deploy the OrganicFoodTraceability contract
-  deployer.deploy(OrganicFoodTraceability);
-};
